03158e2f48680aef1fc82b8ec5a82f38166a6d40
jane3wallace <jane3wallace@gmail.com>
2015-06-20 16:00:22 -0400

    fixed landing image, added more styles, added map script to page 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
 // Roscoe and Jane Biz Site, Week Five Lecture 2 Homework, jQuery
 $(document).ready(function() {
-		
 	
+	function initialize() {
+        var mapCanvas = document.getElementById('map-canvas');
+        var mapOptions = {
+          center: new google.maps.LatLng(34.2192152, -118.6451717),
+          zoom: 17,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
 
 });
